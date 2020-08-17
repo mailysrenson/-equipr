1ee75a3b084e9779bb0a11fb99c7de30f710544a
Lennert Govaerts <lennert_govaerts@hotmail.com>
2020-08-17 16:31:02 +0200

    clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :equipments, only: [:create, :new, :edit, :show, :update, :destroy] do
+    collection do
+      get :search
+    end
+  end
 end
