8c3a783b008e7af4374f41696671f3005ff6ac79
Kristin Gerlach <kristin.gerlach@online.de>
2020-08-19 13:41:48 +0200

    booking on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+Equipment.destroy_all
+User.destroy_all
+
+user = User.create!(
+    email: "mailys_94@hotmail.com",
+    password: 123456,
+    first_name: Mailys,
+    last_name: Renson,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    birthdate: Faker::Date.birthday,
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    bank_account: Faker::Bank.account_number
+  )
+
+4.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    birthdate: Faker::Date.birthday,
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    bank_account: Faker::Bank.account_number
+  )
+end
+
+
+
+20.times do
+  equipment = Equipment.create!(
+    name: ["basketball", "basketball jersey","knee pads", "soccer ball", "goalie gloves", "stop watch",
+      "shin guards", "soccer jersey", "tennis racket", "tennis ball", "badminton racket"].sample,
+    price: Faker::Commerce.price,
+    description: Faker::Lorem.sentence,
+    user_id: (User.all).sample.id
+  )
+end
+
+3.times do
+  booking = Booking.create!(
+    name: ["basketball", "basketball jersey","knee pads", "soccer ball", "goalie gloves", "stop watch",
+      "shin guards", "soccer jersey", "tennis racket", "tennis ball", "badminton racket"].sample,
+    period: Faker::Date.between,
+    rented_from: Faker::Name.name,
+    status:["pending", "declined", "accepted"].sample
+  )
+end
