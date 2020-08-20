710bb29d8a53831f0719c595c6b51bded02b5af2
Lennert Govaerts <lennert_govaerts@hotmail.com>
2020-08-20 14:09:31 +0200

    geocoding map

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -3,10 +3,13 @@ class Equipment < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
   validate :check_minimal_one_picture
+  validates :address, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :price, numericality: true
   validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
    def unavailable_dates
     bookings.pluck(:start_date, :end_date).map do |range|
