316998aeb88b11321f4e1ead1091040dcb92f07f
mailys renson <mailys_94@hotmail.com>
2020-08-21 15:18:49 +0200

    fixed navbar toggle when no user pic

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2020_08_20_093003) do
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "equipment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["equipment_id"], name: "index_favorites_on_equipment_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +106,6 @@ ActiveRecord::Schema.define(version: 2020_08_20_093003) do
   add_foreign_key "bookings", "equipment"
   add_foreign_key "bookings", "users"
   add_foreign_key "equipment", "users"
+  add_foreign_key "favorites", "equipment"
+  add_foreign_key "favorites", "users"
 end
